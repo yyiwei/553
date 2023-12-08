bb0cece787c564214bdd60f845d84a00c2b26ea1
Yunchi <yunchi@umich.edu>
2023-12-08 14:36:01 -0500

    nit auto formatting

--- lr_imbalance/Imbalance_SGD.py
+++ lr_imbalance/Imbalance_SGD.py
@@ -1,32 +1,31 @@
-# EECS 553 project -- SGD with logit adjusted loss
-
 import numpy as np
 
 
 def normalize(data):
-    n,d = data.shape
-    mean = np.mean(data,axis=0).reshape([1,d])
-    std = np.std(data,axis=0).reshape([1,d])
-    
+    n, d = data.shape
+    mean = np.mean(data, axis=0).reshape([1, d])
+    std = np.std(data, axis=0).reshape([1, d])
+
     return (data-mean)/std
 
 
 def sigmoid(a):
-    #a=np.float16(a)
+    # a=np.float16(a)
     return 1/(1+np.exp(-a+1e-12))
 
 
-def Gi(xi,yi,alpha,delta,tau,theta):
-    #input: training data xi: dimension (d+1)*1, yi: 1/-1, theta: [1 w1 ... wd]
-    #output: gradient for all parameters for one iteration
+def Gi(xi, yi, alpha, delta, tau, theta):
+    # input: training data xi: dimension (d+1)*1, yi: 1/-1, theta: [1 w1 ... wd]
+    # output: gradient for all parameters for one iteration
     f = sigmoid(theta.T@xi)
     k = delta**(-yi*tau)*np.exp(-f*yi*tau+1e-12)
-    gxi = alpha/(1+k)*(-yi*tau)*k*(1+np.exp(-theta.T@xi+1e-12))**-2*np.exp(-theta.T@xi+1e-12)*xi
-    
+    gxi = alpha/(1+k)*(-yi*tau)*k*(1+np.exp(-theta.T@xi+1e-12))**- \
+        2*np.exp(-theta.T@xi+1e-12)*xi
+
     return gxi
 
 
-def logit_lr(x_train, y_train, x_test, y_test, step_size, num_epoch,alpha,tau):
+def logit_lr(x_train, y_train, x_test, y_test, step_size, num_epoch, alpha, tau):
     """
     x_train - (n_train, d)
     y_train - (n_train,)
@@ -40,64 +39,61 @@ def logit_lr(x_train, y_train, x_test, y_test, step_size, num_epoch,alpha,tau):
     d = x_train.shape[1]
     n_test = x_test.shape[0]
 
-    #construct x_bar
-    Xtrain = np.concatenate([np.ones([n,1]),x_train],axis=1)
-    Xtest = np.concatenate([np.ones([n_test,1]),x_test],axis=1)
+    # construct x_bar
+    Xtrain = np.concatenate([np.ones([n, 1]), x_train], axis=1)
+    Xtest = np.concatenate([np.ones([n_test, 1]), x_test], axis=1)
 
-    #Find delta
+    # Find delta
     Phy1 = np.count_nonzero(y_train+1)
     delta = Phy1/(n-Phy1)
-    
-    #SGD
-    theta = np.zeros([d+1,1])
-    #num_epoch = 200  #number of epoches we want to run
-    #step_size = 1   #step size for SGD
 
-    for t in range (0,num_epoch):
+    # SGD
+    theta = np.zeros([d+1, 1])
+    # num_epoch = 200  #number of epoches we want to run
+    # step_size = 1   #step size for SGD
+
+    for t in range(0, num_epoch):
         perm = np.random.permutation(n)
-        for i in range(0,n):
-            xi = Xtrain[perm[i],:].reshape(d+1,1)
+        for i in range(0, n):
+            xi = Xtrain[perm[i], :].reshape(d+1, 1)
             yi = y_train[perm[i]]
-            gd = Gi(xi,yi,alpha,delta,tau,theta)
+            gd = Gi(xi, yi, alpha, delta, tau, theta)
             theta = theta - step_size*gd
-        
-        #Find loss every epoch
+
+        # Find loss every epoch
 
         print(theta[0])
-    
-    #Test error
+
+    # Test error
     acc = 0
-    for i in range (0,n_test):
-        f = (theta.T@Xtest[i,:].reshape(d+1,1))
-        #print('f is ',f,'y is',y_test[i])
-        if (f >=0 and y_test[i]>=0) or (f <0 and y_test[i]<0):
-            acc +=1
-        
+    for i in range(0, n_test):
+        f = (theta.T@Xtest[i, :].reshape(d+1, 1))
+        # print('f is ',f,'y is',y_test[i])
+        if (f >= 0 and y_test[i] >= 0) or (f < 0 and y_test[i] < 0):
+            acc += 1
+
     acc = 1-acc/n_test
     return acc
 
 
-#MAIN
-#Find test error
-x_train = np.load("xtrain_im.npy",allow_pickle=True)    # shape (n_train, d)
-x_test = np.load("xtest_im.npy",allow_pickle=True)      # shape (n_test, d)
+# MAIN
+# Find test error
+x_train = np.load("xtrain_im.npy", allow_pickle=True)    # shape (n_train, d)
+x_test = np.load("xtest_im.npy", allow_pickle=True)      # shape (n_test, d)
 
-y_train = np.load("ytrain_im.npy",allow_pickle=True)    # shape (n_train,)
-y_test = np.load("ytest_im.npy",allow_pickle=True)        # shape (n_test,)
+y_train = np.load("ytrain_im.npy", allow_pickle=True)    # shape (n_train,)
+y_test = np.load("ytest_im.npy", allow_pickle=True)        # shape (n_test,)
 
-#Normalize the data
+# Normalize the data
 x_train = normalize(x_train)
 x_test = normalize(x_test)
 
 
-tau=1
-alpha=1
-step_size=0.3
+tau = 1
+alpha = 1
+step_size = 0.3
 num_epoch = 500
 
-acc = logit_lr(x_train, y_train, x_test, y_test, step_size, num_epoch, alpha, tau)
+acc = logit_lr(x_train, y_train, x_test, y_test,
+               step_size, num_epoch, alpha, tau)
 print("test error:", acc)
-
-
-
-
